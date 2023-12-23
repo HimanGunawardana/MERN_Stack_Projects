a5662e3a24ea40e960c01dc20fd73132ece1cc36
Himan Henuka Gunawardana <himanhenuka18@gmail.com>
2023-12-23 23:43:05 +0530

    up1

--- AdminPlannel/testreactapp/src/App.js
+++ AdminPlannel/testreactapp/src/App.js
@@ -1,17 +1,22 @@
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard'
 
 function App() {
 
-  const navigate = useNavigate();
+//  const navigate = useNavigate();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome TeasyPay</h1>
-        <button className='users-button' onClick={() => navigate('/users')}>Users</button>
-      </header>
-    </div>
+    // <div className="App">
+      
+    //   <header className="App-header">
+    //     <h1>Welcome TeasyPay</h1>
+    //     <button className='users-button' onClick={() => navigate('/users')}>Users</button>
+    //   </header>
+      
+    // </div>
+
+    <Dashboard/>
   );
 }
 
