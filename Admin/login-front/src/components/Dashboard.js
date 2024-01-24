41d6921a7bbb78bba56c86be7fd88b11c1d80db3
Himan Henuka Gunawardana <himanhenuka18@gmail.com>
2024-01-24 19:14:11 +0530

    SignIn_SingUp

---
+++ Admin/login-front/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import './App.css';
+// import Login from './Components/Login/Login';
+import Sidebar from './layout/Sidebar/Sidebar';
+import Content from './layout/Content/Content';
+
+function userDetails2() {
+  return (
+    <>
+      <div className='userDetails2'>
+   
+        <Sidebar />
+        <Content />
+        
+      </div>
+    </>
+  )
+}
+
+export default userDetails2
+
+
